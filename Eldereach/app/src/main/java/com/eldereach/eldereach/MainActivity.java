d6b7c60131da5cd250cae97fc9045a3c3b3876c7
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-05-06 16:58:23 +0800

    Add login, signup pages (login persists)

--- Eldereach/app/src/main/java/com/eldereach/eldereach/MainActivity.java
+++ Eldereach/app/src/main/java/com/eldereach/eldereach/MainActivity.java
@@ -13,9 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Objects;
+
+@SuppressWarnings("unchecked")
 public class MainActivity extends AppCompatActivity {
     public EditText emailId, passwd;
     Button btnSignUp;
@@ -27,48 +29,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseAuth = FirebaseAuth.getInstance();
-        emailId = findViewById(R.id.ETemail);
-        passwd = findViewById(R.id.ETpassword);
-        btnSignUp = findViewById(R.id.btnSignUp);
-        signIn = findViewById(R.id.TVSignIn);
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String emailID = emailId.getText().toString();
-                String paswd = passwd.getText().toString();
-                if (emailID.isEmpty()) {
-                    emailId.setError("Provide your Email first!");
-                    emailId.requestFocus();
-                } else if (paswd.isEmpty()) {
-                    passwd.setError("Set your password");
-                    passwd.requestFocus();
-                } else if (emailID.isEmpty() && paswd.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
-                } else if (!(emailID.isEmpty() && paswd.isEmpty()))
-                    firebaseAuth.createUserWithEmailAndPassword(emailID, paswd).addOnCompleteListener(MainActivity.this, new OnCompleteListener() {
-                        @Override
-                        public void onComplete(@NonNull Task task) {
 
-                            if (!task.isSuccessful()) {
-                                Toast.makeText(MainActivity.this.getApplicationContext(),
-                                        "Sign up unsuccessful: " + task.getException().getMessage(),
-                                        Toast.LENGTH_SHORT).show();
-                            } else {
-                                startActivity(new Intent(MainActivity.this, UserActivity.class));
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this, UserActivity.class));
+        } else {
+            emailId = findViewById(R.id.ETemail);
+            passwd = findViewById(R.id.ETpassword);
+            btnSignUp = findViewById(R.id.btnSignUp);
+            signIn = findViewById(R.id.TVSignIn);
+            btnSignUp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String emailID = emailId.getText().toString();
+                    String paswd = passwd.getText().toString();
+                    if (emailID.isEmpty()) {
+                        emailId.setError("Provide your Email first!");
+                        emailId.requestFocus();
+                    } else if (paswd.isEmpty()) {
+                        passwd.setError("Set your password");
+                        passwd.requestFocus();
+                    } else if (emailID.isEmpty() && paswd.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                    } else if (!(emailID.isEmpty() && paswd.isEmpty()))
+                        firebaseAuth.createUserWithEmailAndPassword(emailID, paswd).addOnCompleteListener(MainActivity.this, new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this.getApplicationContext(),
+                                            "Sign up unsuccessful: " + Objects.requireNonNull(task.getException()).getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(MainActivity.this, UserActivity.class));
+                                }
                             }
-                        }
-                    });
-                else {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        });
+                    else {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent I = new Intent(MainActivity.this, ActivityLogin.class);
-                startActivity(I);
-            }
-        });
+            });
+            signIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent I = new Intent(MainActivity.this, ActivityLogin.class);
+                    startActivity(I);
+                }
+            });
+        }
     }
 }
