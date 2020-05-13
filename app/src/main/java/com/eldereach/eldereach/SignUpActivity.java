ad4b92abd38bbe2266d08794840733f7753d8a94
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-05-13 23:03:35 +0800

    Test commit

--- app/src/main/java/com/eldereach/eldereach/SignUpActivity.java
+++ app/src/main/java/com/eldereach/eldereach/SignUpActivity.java
@@ -21,7 +21,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import static android.widget.CompoundButton.*;
+import static android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class SignUpActivity extends AppCompatActivity {
     EditText email;
